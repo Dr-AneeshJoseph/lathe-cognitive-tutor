88852a96ad7cb456a2e84698bbcaec8f6c867c74
Dr. Aneesh Joseph <aneeshjoseph1091@gmail.com>
2025-12-16 20:50:24 +0530

    Create deconstructor.py

---
+++ faculty/deconstructor.py
@@ -0,0 +1,8 @@
+class Deconstructor:
+    def __init__(self):
+        self.name = "The Deconstructor"
+        self.description = "Inverts 'common sense' to reveal hidden ideology."
+    
+    def teach(self, query):
+        return f"[{self.name}]: You think this is a natural law? Historically, this concept didn't exist until 1850..."
+      
